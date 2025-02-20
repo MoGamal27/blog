75f6d7fb0b0c2e76e06d8869f621d54389dd8b25
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-02-20 15:04:51 +0200

    update(Post model): add attribute published

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,16 @@
 from django.db import models
-
+from django.utils import timezone
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
     body = models.TextField()
+    published = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     
+    class Mete:
+        ordering = ['-published']
     
     def __str__(self):
         return self.title
