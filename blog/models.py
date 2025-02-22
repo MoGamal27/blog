8d14e8dfc3dcede53d77fa31d3af36fc09fb4da2
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-02-22 14:50:53 +0200

    fix issue arguments

--- blog/models.py
+++ blog/models.py
@@ -34,5 +34,8 @@ class Post(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse("blog:post_detail", args= [self.year, self.month, self.day, self.slug])
+        return reverse("blog:post_detail", args= [self.publish.year, 
+                                                  self.publish.month, 
+                                                  self.publish.day, 
+                                                  self.slug])
     
